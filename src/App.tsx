2e12eca402687b74c8984a3e321331e139036b3c
Gideon Spierings <gideon@allianceblock.io>
2021-09-28 15:55:32 +0300

    Initial i18next implementation

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,42 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import i18n from 'i18next';
+
+import { Trans, useTranslation } from 'react-i18next';
 
 function App() {
+  const { t } = useTranslation();
+  
+  const onChangeLanguage = (event: any) => {
+    i18n.changeLanguage(event.target.value);
+  };
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+        <h1>React i18next</h1>
+
+        <select onChange={onChangeLanguage}>
+          <option value="en">English</option>
+          <option value="nl">Nederlands</option>
+        </select>
+
+        <h2>Simple</h2>
+        <p>{t('teamMember')}</p>
+        
+        <h2>Plural</h2>
+        <p>{t('teamMember', { count: 2 })}</p>
+
+        <h2><code>Trans</code> component</h2>
         <p>
-          Edit <code>src/App.tsx</code> and save to reload.
+          <Trans i18nKey="teamMember">Fallback text here</Trans>
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        
+        <h2>Nested</h2>
+        <p>{t('errors.404')}</p>
+        
+        <h2>Context</h2>
+        <p>{t('gender', { context: 'male' })}</p>
       </header>
     </div>
   );
